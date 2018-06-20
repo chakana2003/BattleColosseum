bf4d27a988f5f5e64a2d7089b6d0555d96edb6c8
chakana2003 <chakana2003@gmail.com>
2018-06-20 18:39:08 +0900

    Lobby 에서 Play 맵으로 이동 로직 구현. 했지만 되지않음. TEMP 랑 똑같이 만들어도 되지 않음.

--- Source/BattleColosseum/Play/Warrior/C_WarriorCharacter.cpp
+++ Source/BattleColosseum/Play/Warrior/C_WarriorCharacter.cpp
@@ -1,35 +1,149 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "C_WarriorCharacter.h"
-#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/SkeletalMeshComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "ConstructorHelpers.h"
+#include "Engine/SkeletalMesh.h"
+#include "UnrealNetwork.h"
+#include "Components/InputComponent.h"
+#include "Components/TextRenderComponent.h"
 
 
-// Sets default values
-AC_WarriorCharacter::AC_WarriorCharacter()
-{
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+AC_WarriorCharacter::AC_WarriorCharacter() {
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	UserID = CreateDefaultSubobject<UTextRenderComponent>(TEXT("UserID"));
+
+	SpringArm->AttachToComponent(GetCapsuleComponent(), FAttachmentTransformRules::KeepRelativeTransform);
+	Camera->AttachToComponent(SpringArm, FAttachmentTransformRules::KeepRelativeTransform);
+	UserID->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform);
+
+	GetMesh()->SetRelativeLocationAndRotation(FVector(0.f, 0.f, -88.f), FRotator(0.f, -90.f, 0.f));
+
+	bReplicates = true;
+
+	// 변수 초기화
+	IsSprinting = false;
+	SprintRate = 100.f;
+	RightView = false;
+
+	this->bUseControllerRotationPitch = false;				// 플레이어가 위아래로 돌아가지 않도록
+	SpringArm->bUsePawnControlRotation = true;				// 컨트롤러의 회전값을 사용하도록
+	SpringArm->bInheritPitch = true;						// 카메라만 위아래로 돌아가도록
+	SpringArm->SocketOffset = FVector(0.f, 50.f, 70.f);		// 오른쪽 어깨위로 설정
+	SpringArm->TargetArmLength = 150.f;						// 카메라 거리 가깝게 설정
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->bEnableCameraRotationLag = true;
+	SpringArm->CameraLagSpeed = 9.0f;
+	SpringArm->CameraRotationLagSpeed = 9.0f;
+
+	GetCapsuleComponent()->SetNotifyRigidBodyCollision(true);  // hit 이벤트 활성화(옛날이름)
+	GetCapsuleComponent()->SetCapsuleHalfHeight(88.f);
+	GetCapsuleComponent()->SetCapsuleRadius(34.f);
+
+	UserID->SetRelativeLocation(FVector(0.f, 0.f, 200.f));
+	UserID->SetRelativeRotation(FRotator(0.f, 90.f, 0.f));
+	UserID->HorizontalAlignment = EHorizTextAligment::EHTA_Center;
+	UserID->SetText(FText::FromString(TEXT("UserID")));
+	UserID->SetIsReplicated(true);
 }
 
-// Called when the game starts or when spawned
 void AC_WarriorCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Ori_Speed = 400.f;
+	Sprint_Speed = Ori_Speed * SprintRate;
 }
 
-// Called every frame
+void AC_WarriorCharacter::MoveForward(float Value)
+{
+	if (Value != 0.f) {
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
+}
+
+void AC_WarriorCharacter::MoveRight(float Value)
+{
+	if (Value != 0.f) {
+		AddMovementInput(GetActorRightVector(), Value);
+	}
+}
+
+bool AC_WarriorCharacter::ActiveSprint_Validate() {
+	return true;
+}
+
+void AC_WarriorCharacter::ActiveSprint_Implementation() {
+	if (!IsSprinting)
+		IsSprinting = true;
+}
+
+bool AC_WarriorCharacter::DeActiveSprint_Validate() {
+	return true;
+}
+
+void AC_WarriorCharacter::DeActiveSprint_Implementation() {
+	if (IsSprinting)
+		IsSprinting = false;
+}
+
+void AC_WarriorCharacter::SwitchView()
+{
+	if (RightView) {
+		RightView = false;
+		SpringArm->SocketOffset = FVector(0.f, -50.f, 70.f);
+	}
+	else {
+		RightView = true;
+		SpringArm->SocketOffset = FVector(0.f, 50.f, 70.f);
+	}
+}
+
 void AC_WarriorCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (IsSprinting) {
+		// 캐릭터 무브먼트 컴포넌트의 Max Speed 값을 Sprint_Speed로 바꿈
+		GetCharacterMovement()->MaxWalkSpeed = Sprint_Speed;
+	}
+	else {
+		// 캐릭터 무브먼트 컴포넌트의 Max Speed 값을 Ori_Speed로 바꿈
+		GetCharacterMovement()->MaxWalkSpeed = Ori_Speed;
+	}
 
 }
 
-// Called to bind functionality to input
-void AC_WarriorCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void AC_WarriorCharacter::SetupPlayerInputComponent(UInputComponent * PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("K_MoveForward", this, &AC_WarriorCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("K_MoveRight", this, &AC_WarriorCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("K_LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("K_Turn", this, &APawn::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("K_Jump", IE_Pressed, this, &AC_WarriorCharacter::Jumpp);
+
+	PlayerInputComponent->BindAction("K_Sprint", IE_Pressed, this, &AC_WarriorCharacter::ActiveSprint);
+	PlayerInputComponent->BindAction("K_Sprint", IE_Released, this, &AC_WarriorCharacter::DeActiveSprint);
+
+	PlayerInputComponent->BindAction("K_SwitchView", IE_Pressed, this, &AC_WarriorCharacter::SwitchView);
+}
+
+void AC_WarriorCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Replicate to everyone
+	DOREPLIFETIME(AC_WarriorCharacter, IsSprinting);
+}
+
+void AC_WarriorCharacter::Jumpp() {
+	Jump();
 }
 
--- Source/BattleColosseum/Play/Warrior/C_WarriorCharacter.h
+++ Source/BattleColosseum/Play/Warrior/C_WarriorCharacter.h
@@ -15,19 +15,55 @@ public:
 	// Sets default values for this character's properties
 	AC_WarriorCharacter();
 
-protected:
-	// Called when the game starts or when spawned
+	// 변수
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		class USpringArmComponent* SpringArm;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		class UTextRenderComponent* UserID;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+		class UCameraComponent* Camera;
+
+	// RunningState, 뛰기 불리언.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Code", Replicated)
+		bool IsSprinting;
+	//  Run Rate, 뛰기 비율.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Code")
+		float SprintRate;
+	// OriginSpeed, 원래 속도.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Code")
+		float Ori_Speed;
+	// SprintSpeed, 변한 속도. 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Code")
+		float Sprint_Speed;
+	// Look Over Right Shoulder, 오른쪽어깨로 보고있니. 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Code")
+		bool RightView;
+
+public: // 함수
+
 	virtual void BeginPlay() override;
 
-public:	
+	// 입력 관련 행동 함수
+	UFUNCTION(BlueprintCallable)
+		void MoveForward(float Value);						// 앞으로 가기
+	UFUNCTION(BlueprintCallable)
+		void MoveRight(float Value);						// 옆으로 가기
+	UFUNCTION(BlueprintCallable, Server, reliable, WithValidation)
+		void ActiveSprint();								// 뛰기 활성화
+	UFUNCTION(BlueprintCallable, Server, reliable, WithValidation)
+		void DeActiveSprint();								// 뛰기 비활성화
+	UFUNCTION()
+		void SwitchView();
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-public:  // 변수
+	void GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const override;
 
-public:  // 함수
+	void Jumpp();
 	
 };
